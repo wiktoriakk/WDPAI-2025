d141a3178bf58178e4ecfe1f12f8a50e184baaeb
Wiktoria Kosek <kosek.wiktoria1@gmail.com>
2025-10-13 08:29:59 +0200

    lab2

---
+++ Routing.php
@@ -0,0 +1,17 @@
+<?php
+
+class Routing {
+    public static function run(string $path) {
+        switch ($path) {
+    case 'login':
+        include 'public/views/login.html';
+        break;
+    case 'dashboard':
+        include 'public/views/dashboard.html';
+        break;
+    default:
+        include 'public/views/404.html';
+        break;
+    }
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "<h1>Hi Students! </h1>";
+require_once 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::run($path);
